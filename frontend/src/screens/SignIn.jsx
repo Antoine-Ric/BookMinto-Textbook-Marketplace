6a6e9f8885c30115d0b964b7fab8870f5b1d2705
akelpie <jdmasopeh@Outlook.com>
2024-01-21 19:13:37 -0500

    Did the signup page. this should work but last time I checked it didn't. I'll have to check again.

--- frontend/src/screens/SignIn.jsx
+++ frontend/src/screens/SignIn.jsx
@@ -1,10 +1,52 @@
 // import navbar from '../components/navbar'
 
+
 export default function SignIn() {
-    return(
-        <>
-            <navbar />
-            <h1>This will be the sign in page</h1>
-        </>
-    )
+    return (
+      
+
+        <div className="signup-page">
+          <div className="signup-images"></div>
+          <div className="signup-form-container">
+            <div className="signup-form-header">
+              <h1>Sign up</h1>
+              {/* <p>Step 1 of 3</p> */}
+            </div>
+            <form className="signup-form">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-field"
+                  placeholder="First name"
+                />
+                <input
+                  type="text"
+                  className="form-field"
+                  placeholder="Last name"
+                />
+              </div>
+              <input type="email" placeholder="Email" />
+              <input type="text" placeholder="Username" />
+              <input type="password" placeholder="Password" />
+
+              <div className="password_requirements">
+                <ul>
+                  <li>X Must be at least 8 characters</li>
+                  <li>X Must include a number and special character</li>
+                </ul>
+              </div>
+
+
+              <button type="submit">Next</button>
+            </form>
+            <p className="policy_footer">
+              By clicking Next you agree to our
+              <a href="/terms"> Terms of Service </a>
+              and
+              <a href="/privacy"> Privacy Policy</a>.
+            </p>
+          </div>
+        </div>
+      
+    );
 }
