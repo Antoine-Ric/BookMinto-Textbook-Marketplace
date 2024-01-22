81f2f8eac38861258b2d5fd7b11218606300c20d
AngelaF81210 <angelogenshin2933@gmail.com>
2024-01-21 22:09:59 -0500

    sign in tempalte done

--- frontend/src/screens/SignIn.jsx
+++ frontend/src/screens/SignIn.jsx
@@ -1,5 +1,7 @@
 // import navbar from '../components/navbar'
 
+import React from "react";
+import "../styles/SignIn.css";
 
 export default function SignIn() {
     return (
@@ -9,42 +11,14 @@ export default function SignIn() {
           <div className="signup-images"></div>
           <div className="signup-form-container">
             <div className="signup-form-header">
-              <h1>Sign up</h1>
-              {/* <p>Step 1 of 3</p> */}
+              <h1>Sign in</h1>
             </div>
             <form className="signup-form">
-              <div className="form-group">
-                <input
-                  type="text"
-                  className="form-field"
-                  placeholder="First name"
-                />
-                <input
-                  type="text"
-                  className="form-field"
-                  placeholder="Last name"
-                />
-              </div>
               <input type="email" placeholder="Email" />
               <input type="text" placeholder="Username" />
               <input type="password" placeholder="Password" />
-
-              <div className="password_requirements">
-                <ul>
-                  <li>X Must be at least 8 characters</li>
-                  <li>X Must include a number and special character</li>
-                </ul>
-              </div>
-
-
-              <button type="submit">Next</button>
+              <button type="submit">Login</button>
             </form>
-            <p className="policy_footer">
-              By clicking Next you agree to our
-              <a href="/terms"> Terms of Service </a>
-              and
-              <a href="/privacy"> Privacy Policy</a>.
-            </p>
           </div>
         </div>
       
