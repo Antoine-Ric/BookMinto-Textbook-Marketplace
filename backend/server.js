d66ff9e99ed1f8deea16940aaf30fc926ab8fd23
RaulGonzal <Raulgonzalc@yahoo.com>
2024-01-31 23:40:01 -0500

    bringing product routes into server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 import connectDB from './config/db.js'
 import {notFound, errorHandler} from './middleware/errorMiddleware.js'
-//import productRoutes from './routes/productRoutes.js'
+import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 
 const port = process.env.PORT || 5001
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
-//app.use('/api/products', productRoutes)
+app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 
 app.use(notFound)
