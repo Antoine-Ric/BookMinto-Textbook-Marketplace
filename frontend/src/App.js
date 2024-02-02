244a8ea2b7a57b21bd66fa29e5a3d105431bf2f2
akelpie <jdmasopeh@Outlook.com>
2024-02-02 15:25:51 -0500

    Worked on getting the routers to work. The products and Sign In should now have a router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
+import { Outlet } from 'react-router-dom';
 
 
 import { useState } from "react";
@@ -17,7 +17,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Outlet />
         </Container>
 
         {/* <div className="Search-bar-container">
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,34 @@
 import { Navbar, Nav, Container} from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
+import logo from "../assets/logo.png"; // Angela change the logo to have just the leaf
 
 const Header = () => {
   return (
     <header>
-        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-            <Container>
-                <Navbar.Brand href="/">BookMinto</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className = "ms-auto">
-                        {/* <Nav.Link href="/cart"><FaShoppingCart/> Cart</Nav.Link> */}
-                        <Nav.Link href="/login"><FaUser/> Sign In</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-                    
-            </Container>
-        </Navbar>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img src={logo} alt="BookMinto" />
+              BookMinto
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              {/* <Nav.Link href="/cart"><FaShoppingCart/> Cart</Nav.Link> */}
+              <LinkContainer to="/login">
+                <Nav.Link href="/login">
+                  <FaUser /> Sign In
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
-  )
+  );
 }
 
 export default Header
