d79fb69e337146f284a807eac8333eeb0367a0f7
akelpie <jdmasopeh@Outlook.com>
2024-02-09 23:43:12 -0500

    prepare sample data

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,21 +1,23 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 
 const users = [
   {
-    name: 'Admin User',
-    email: 'admin@email.com',
-    password: bcrypt.hashSync('123456', 10),
+    name: "Admin User",
+    email: "admin@email.com",
+    password: bcrypt.hashSync("123456", 10), // 10 is the salt - the higer the number the more secure, the longer it will take
     isAdmin: true,
   },
   {
-    name: 'John Doe',
-    email: 'john@email.com',
-    password: bcrypt.hashSync('123456', 10),
+    name: "John Doe",
+    email: "john@email.com",
+    password: bcrypt.hashSync("123456", 10), // 10 is the salt - the higer the number the more secure, the longer it will take
+    isAdmin: false,
   },
   {
-    name: 'Jane Doe',
-    email: 'jane@email.com',
-    password: bcrypt.hashSync('123456', 10),
+    name: "Jane Doe",
+    email: "jane@email.com",
+    password: bcrypt.hashSync("123456", 10), // 10 is the salt - the higer the number the more secure, the longer it will take
+    isAdmin: false,
   },
 ];
 
