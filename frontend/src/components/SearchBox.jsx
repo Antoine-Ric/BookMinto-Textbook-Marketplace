ea61bac8dba5e129e456e8f8800fcd5adc580e7d
RaulGonzal <Raulgonzalc@yahoo.com>
2024-02-17 13:16:14 -0500

    submit handler

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -3,13 +3,25 @@ import { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import { useParams, useNavigate } from 'react-router-dom'
 
+    /*
+        later on change so there is no need for a search button
+        it will change automatically when a change is made inside
+        of the search bar itself
+    */
+
 const SearchBox = () => {
     const navigate = useNavigate()
     const {keyword: urlKeyword} = useParams()
     const [keyword, setKeyword] = useState(urlKeyword || '')
 
     const submitHandler = (e) => {
-        
+        e.preventDefault()
+        if (keyword.trim()){
+            navigate(`/search/${keyword}`)
+        }
+        else {
+            navigate('/')
+        }
     }
   
     return (
