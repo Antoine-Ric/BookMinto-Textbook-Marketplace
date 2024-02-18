321bbc342e3606a49baf3d9adc58e51dfbc682f5
akelpie <jdmasopeh@outlook.com>
2024-02-18 18:55:10 -0500

    shipping page, search bar, favorites page currently working

--- frontend/src/screens/FavoriteScreen.jsx
+++ frontend/src/screens/FavoriteScreen.jsx
@@ -7,7 +7,6 @@ import {
     Image,
     Form,
     Button,
-    Card,
   } from 'react-bootstrap';
 import { FaTrash } from 'react-icons/fa';
 import Message from '../components/Message';
@@ -28,17 +27,14 @@ const FavoriteScreen = () => {
         dispatch(removeFromCart(id));
       };
 
-      // const checkoutHandler = () => {
-      //   navigate('/login?redirect=/shipping');
-      // };
 
     return (
         <Row>
           <Col md={8}>
-            <h1 style={{ marginBottom: '20px' }}>Shopping Cart</h1>
+            <h1 style={{ marginBottom: '20px' }}>Favorites List</h1>
             {cartItems.length === 0 ? (
               <Message>
-                Your cart is empty <Link to='/'>Go Back</Link>
+                Your favorites list is empty <Link to='/'>Go Back</Link>
               </Message>
             ) : (
               <ListGroup variant='flush'>
@@ -81,32 +77,6 @@ const FavoriteScreen = () => {
                 ))}
               </ListGroup>
             )}
-          </Col>
-          <Col md={4}>
-            <Card>
-              <ListGroup variant='flush'>
-                <ListGroup.Item>
-                  <h2>
-                    Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
-                    items
-                  </h2>
-                  $
-                  {cartItems
-                    .reduce((acc, item) => acc + item.qty * item.price, 0)
-                    .toFixed(2)}
-                </ListGroup.Item>
-                {/* <ListGroup.Item>
-                  <Button
-                    type='button'
-                    className='btn-block'
-                    disabled={cartItems.length === 0}
-                    onClick={checkoutHandler}
-                  >
-                    Proceed To Checkout
-                  </Button>
-                </ListGroup.Item> */}
-              </ListGroup>
-            </Card>
           </Col>
         </Row>
       );
