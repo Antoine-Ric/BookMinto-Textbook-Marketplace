28ec4d5e33c65b33cb4184e9fe7df3856172617f
akelpie <jdmasopeh@outlook.com>
2024-02-19 15:21:50 -0500

    removed the word favorites for a simplier design

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -32,7 +32,7 @@ const Header = () => {
         <Container>
           <LinkContainer to="/">
             <Navbar.Brand>
-              <img src={logo} alt="ProShop" />
+              {/* <img src={logo} alt="ProShop" /> */}
               BookMinto
             </Navbar.Brand>
           </LinkContainer>
@@ -42,7 +42,7 @@ const Header = () => {
             <Nav className="ms-auto">
               <LinkContainer to="/favorites">
                 <Nav.Link>
-                  <FaHeart /> Favorites
+                  <FaHeart />
                   {cartItems.length > 0 && (
                     <Badge pill bg="success" style={{ marginLeft: "5px" }}>
                       {cartItems.reduce((a, c) => a + c.qty, 0)}
