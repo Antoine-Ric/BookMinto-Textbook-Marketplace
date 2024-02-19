ac313498ab46b7a0afa1e4885af8b074ba649665
akelpie <jdmasopeh@outlook.com>
2024-02-19 13:41:22 -0500

    version 1 hearts outline on favorites page

--- frontend/src/screens/FavoriteScreen.jsx
+++ frontend/src/screens/FavoriteScreen.jsx
@@ -1,54 +1,69 @@
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-    Row,
-    Col,
-    ListGroup,
-    Image,
-    Form,
-    Button,
-  } from 'react-bootstrap';
-import { FaTrash } from 'react-icons/fa';
-import Message from '../components/Message';
-import {addToCart, removeFromCart} from '../slices/favoriteSlice';
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Col, ListGroup, Image, Form, Button } from "react-bootstrap";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { FaTrash } from "react-icons/fa";
+import Message from "../components/Message";
+import { addToCart, removeFromCart } from "../slices/favoriteSlice";
 
 const FavoriteScreen = () => {
-    //const navigate = useNavigate();
-    const dispatch = useDispatch();
-  
-    const cart = useSelector((state) => state.cart);
-    const { cartItems } = cart;
+  //const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const addToCartHandler = async (product, qty) => {
-        dispatch(addToCart({ ...product, qty }));
-      };
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
 
-      const removeFromCartHandler = (id) => {
-        dispatch(removeFromCart(id));
-      };
+  // State to track items removed from favorites before page reload
+  const [removedFavorites, setRemovedFavorites] = useState({});
 
+  const addToCartHandler = async (product, qty) => {
+    dispatch(addToCart({ ...product, qty }));
+  };
 
-    return (
-        <Row>
-          <Col md={8}>
-            <h1 style={{ marginBottom: '20px' }}>Favorites List</h1>
-            {cartItems.length === 0 ? (
-              <Message>
-                Your favorites list is empty <Link to='/'>Go Back</Link>
-              </Message>
-            ) : (
-              <ListGroup variant='flush'>
-                {cartItems.map((item) => (
-                  <ListGroup.Item key={item._id}>
-                    <Row>
-                      <Col md={4}>
-                        <Image src={item.image} alt={item.name} fluid rounded />
-                      </Col>
-                      <Col md={5}>
-                        <Link to={`/product/${item._id}`}>{item.name}</Link>
-                      </Col>
-                      {/* <Col md={2}>${item.price}</Col> */}
-                      {/* <Col md={2}>
+  const removeFromCartHandler = (id) => {
+    // Mark the item as removed in local state and localStorage
+    setRemovedFavorites((prev) => {
+      const updated = { ...prev, [id]: true };
+      localStorage.setItem("removedFavorites", JSON.stringify(updated));
+      return updated;
+    });
+  };
+
+  useEffect(() => {
+    // On component mount, check for items marked for removal and remove them from the cart
+    const removedItems = JSON.parse(
+      localStorage.getItem("removedFavorites") || "{}"
+    );
+    Object.keys(removedItems).forEach((id) => {
+      dispatch(removeFromCart(id));
+    });
+
+    // Optionally, clear the marked items from localStorage if you don't want them to be removed on subsequent reloads
+    localStorage.removeItem("removedFavorites");
+  }, [dispatch]);
+
+  return (
+    <Row>
+      <Col md={8}>
+        <h1 style={{ marginBottom: "20px" }}>Favorites List</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Your favorites list is empty <Link to="/">Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant="flush">
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item._id}>
+                <Row>
+                  <Col md={4}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={5}>
+                    <Link to={`/product/${item._id}`}>{item.name}</Link>
+                  </Col>
+                  {/* <Col md={2}>${item.price}</Col> */}
+                  {/* <Col md={2}>
                         <Form.Control
                           as='select'
                           value={item.qty}
@@ -63,25 +78,28 @@ const FavoriteScreen = () => {
                           ))}
                         </Form.Control>
                       </Col> */}
-                      <Col md={3}>
-                        <Button
-                          type='button'
-                          variant='light'
-                          onClick={() => removeFromCartHandler(item._id)}
-                        >
-                          <FaTrash />
-                        </Button>
-                      </Col>
-                    </Row>
-                  </ListGroup.Item>
-                ))}
-              </ListGroup>
-            )}
-          </Col>
-        </Row>
-      );
-    };
-    
-    
+                  <Col md={3}>
+                    <Button
+                      type="button"
+                      variant="light"
+                      onClick={() => removeFromCartHandler(item._id)}
+                    >
+                      {/* Show outlined heart if item is marked as removed, filled otherwise */}
+                      {removedFavorites[item._id] ? (
+                        <FaRegHeart />
+                      ) : (
+                        <FaHeart />
+                      )}
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+    </Row>
+  );
+};
 
 export default FavoriteScreen;
