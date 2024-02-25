5d5b8b125a40c497b6e37a86c0b2993ba26b59e4
Jasmine Masopeh <jdmasopeh@outlook.com>
2024-02-25 15:04:31 -0500

    template profile page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -19,6 +19,7 @@ import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import FavoriteScreen from "./screens/FavoriteScreen";
 import ShippingScreen from './screens/ShippingScreen';
+import ProfileScreen from "./screens/ProfileScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -32,6 +33,7 @@ const router = createBrowserRouter(
 
       <Route path='' element={<PrivateRoute />}>
       <Route path='/shipping' element={<ShippingScreen />} />
+      <Route path="/profile" element={<ProfileScreen />} />
     </Route>
 
     </Route>
